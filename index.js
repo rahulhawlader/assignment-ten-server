b40abc80e19f90cefd576a46ecf6b5328a4d2e6f
Rahulhawlader <hawladerrahul8@gmail.com>
2022-05-05 02:07:57 +0600

    update details

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const { listen } = require('express/lib/application');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -26,6 +26,14 @@ async function run() {
             const cursor = dressCollection.find(query);
             const dresses = await cursor.toArray();
             res.send(dresses)
+        })
+
+
+        app.get('/dress/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const dress = await dressCollection.findOne(query);
+            res.send(dress);
         })
     }
     finally {
