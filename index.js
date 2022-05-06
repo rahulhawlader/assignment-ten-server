38e3b4564484c4fbd7abf65dfd9c24dea88ce52a
Rahulhawlader <hawladerrahul8@gmail.com>
2022-05-06 06:01:45 +0600

    delete api

--- index.js
+++ index.js
@@ -52,6 +52,14 @@ async function run() {
             res.send(result)
 
         })
+
+
+        app.delete('/dress/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await dressCollection.deleteOne(query);
+            res.send(result)
+        })
     }
     finally {
 
